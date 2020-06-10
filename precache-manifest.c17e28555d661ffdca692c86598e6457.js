fe2d899d4b4d988e6de94b3a5606d8a7d77d7cc6
vabs <njoy.today@gmail.com>
2020-06-09 23:03:10 -0400

    Updates

---
+++ precache-manifest.c17e28555d661ffdca692c86598e6457.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd0be5e116c0748ce381f421a70cb5f",
+    "url": "/fortune-cookie/index.html"
+  },
+  {
+    "revision": "1c6d7417032db6170e58",
+    "url": "/fortune-cookie/static/css/main.14e47d8e.chunk.css"
+  },
+  {
+    "revision": "60ff0a7c02b4c0b61e3d",
+    "url": "/fortune-cookie/static/js/2.5bce8356.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fortune-cookie/static/js/2.5bce8356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c6d7417032db6170e58",
+    "url": "/fortune-cookie/static/js/main.ae9de60a.chunk.js"
+  },
+  {
+    "revision": "26a84b96e44c6803e46a",
+    "url": "/fortune-cookie/static/js/runtime-main.d08f8bb8.js"
+  },
+  {
+    "revision": "beabd2489a98567acad6d215a17f2ed1",
+    "url": "/fortune-cookie/static/media/cookie.beabd248.png"
+  }
+]);
